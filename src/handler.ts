8a79395620c718a7f33ff5321aeae15354c23038
Mathis Bullinger <mathis@bullinger.dev>
2020-03-09 18:38:13 +0100

    feat: process images added to s3

--- src/handler.ts
+++ src/handler.ts
@@ -5,5 +5,12 @@ export const resize = async ({ Records, path, body }) => {
     const paths = JSON.parse(body)?.imgs
     if (!Array.isArray(paths)) return
     await process(...paths)
+  } else if (Records.length) {
+    const added = Records.filter(
+      ({ eventName }) => eventName === 'ObjectCreated:Put'
+    )
+      .map(({ s3 }) => s3.object.key)
+      .filter(key => /^\/?[^/]+\/?$/.test(key))
+    if (added.length) await process(...added)
   }
 }
