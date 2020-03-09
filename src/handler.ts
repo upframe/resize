5c0ee64e921abc28ecc00685c71db642d85613a7
Mathis Bullinger <mathis@bullinger.dev>
2020-03-09 21:39:19 +0100

    build: include sharp bundle

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
 import process from './resize'
 
 export const resize = async ({ Records, path, body }) => {
-  if (path.replace(/^\/|\/$/g, '') === 'resize') {
+  if (path?.replace(/^\/|\/$/g, '') === 'resize') {
     const paths = JSON.parse(body)?.imgs
     if (!Array.isArray(paths)) return
     await process(...paths)
