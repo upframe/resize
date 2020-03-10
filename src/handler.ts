fa134a0b1eb44948240b781926fb86bcaeb78d18
Mathis Bullinger <mathis@bullinger.dev>
2020-03-10 19:03:53 +0100

    ref: cleanup

--- src/handler.ts
+++ src/handler.ts
@@ -2,21 +2,19 @@ import processImg from './resize'
 
 export const resize = async ({ Records, path, body }, context) => {
   context.callbackWaitsForEmptyEventLoop = false
+  let added: string[] = []
 
   if (path?.replace(/^\/|\/$/g, '') === 'resize') {
     const paths = JSON.parse(body)?.imgs
-    if (!Array.isArray(paths)) return
-    await processImg(...paths)
+    if (Array.isArray(paths)) added = paths
   } else if (Records.length) {
     if (process.env.stage !== 'prod')
       return void console.log(
         `ignoring event because stage is ${process.env.stage}`
       )
-    const added = Records.filter(
-      ({ eventName }) => eventName === 'ObjectCreated:Put'
-    )
+    added = Records.filter(({ eventName }) => eventName === 'ObjectCreated:Put')
       .map(({ s3 }) => s3.object.key)
       .filter(key => /^\/?[^/]+\/?$/.test(key))
-    if (added.length) await processImg(...added)
   }
+  if (added.length) await processImg(...added)
 }
